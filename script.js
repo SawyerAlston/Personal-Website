674c9e11cce40e621b1ba2ce5b7b2dd9ef0dfac0
SawyerAlston <sawyeralston06@gmail.com>
2025-08-08 23:50:23 -0500

    Add scrolling animations

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+    threshold: 0.1
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+        }
+    });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+    });
+});
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
